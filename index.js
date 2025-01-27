90a16e94d7e4b55d4785dea3bb50b8a9561a491d
matheusaraujoc <theus461001@gmail.com>
2025-01-27 08:44:48 -0300

    index update

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
-const animeUrl = "https://animefire.plus";
-
-async function testConnection() {
-  const url = animeUrl; // Defina a URL que você deseja acessar.
-
+async function checkConnection() {
+  const url = 'https://animefire.plus'; // Substitua pelo URL correto
   try {
-    const response = await axios.get(url);
-
-    if (response.status === 200) {
-      console.log('Conectado ao site com sucesso!');
-      console.log('Status da resposta:', response.status);
-    } else {
-      console.error('Falha ao se conectar. Status:', response.status);
-    }
-  } catch (err) {
-    console.error('Erro ao tentar se conectar ao site:', err.message);
+    const response = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+      }
+    });
+    console.log('Conectado ao site com sucesso!');
+    console.log('Status da resposta:', response.status);
+  } catch (error) {
+    console.error('Erro ao tentar se conectar ao site:', error.response ? error.response.status : error.message);
   }
 }
 
-testConnection();
+checkConnection();
